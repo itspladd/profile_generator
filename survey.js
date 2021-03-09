842efb31d554a7c5cea106eb525070a908d45d46
Paul Ladd <vlendle@gmail.com>
2021-03-09 19:53:27 +0000

    Adding test code to make sure readline works.

--- survey.js
+++ survey.js
@@ -1,1 +1,12 @@
 const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("Who lives in a pineapple under the sea? ", (answer) => {
+  console.log(`Wrong. ${answer} lives in my head, rent-free.`);
+
+  rl.close();
+});
