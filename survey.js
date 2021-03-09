da2f70aa1436c3b112a68b50db68b0cf63ab57c1
Paul Ladd <vlendle@gmail.com>
2021-03-09 20:09:17 +0000

    First question works!

--- survey.js
+++ survey.js
@@ -5,8 +5,24 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question("Who lives in a pineapple under the sea? ", (answer) => {
-  console.log(`Wrong. ${answer} lives in my head, rent-free.`);
 
-  rl.close();
-});
+const askQuestion = (survey, questionNum) => {
+  rl.question(survey[questionNum].question + " ", (answer) => {
+    survey[questionNum]["answer"] = answer;
+    console.log(survey);
+    rl.close();
+  });
+};
+
+const survey = [
+  { question: `What's your name? Nicknames fine too!` },
+  { question: `What are your pronouns?` },
+  { question: `What's an activity you like doing?` },
+  { question: `What do you listen to while doing that?` },
+  { question: `Which meal is your favourite (eg: dinner, brunch, etc.)?` },
+  { question: `What's your favourite thing to eat for that meal?` },
+  { question: `Which sport is your absolute favourite?` },
+  { question: `What is your superpower? In a few words, tell us what you are amazing at!` },
+];
+
+askQuestion(survey, 0);
